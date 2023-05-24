ed24bc297fa7b1e4e800d289db81f4f928d3cb3c
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-24 11:03:24 +0100

    urls

---
+++ src/consts.js
@@ -0,0 +1,3 @@
+export const KEY = process.env.REACT_APP_API_KEY;
+export const BASE_URL = "http://api.openweathermap.org/data/2.5";
+export const COUNTRY_URL = "https://ajayakv-rest-countries-v1.p.rapidapi.com/rest/v1/all";
