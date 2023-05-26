4c0cb19bdd6ad9f2df38b039ce776275d5ca726e
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-26 08:36:04 +0100

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cryptos from "./components/Cryptos";
+// eslint-disable-next-line import/no-cycle
+import Navbar from "./components/Navbar";
 
 export default function App() {
+  const [dark, setDark] = useState(true);
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Cryptos />} />
-      </Routes>
+      <div className={`${dark ? "dark" : ""}`}>
+        <Navbar dark={dark} setDark={setDark} />
+        <Routes>
+          <Route path="/" element={<Cryptos />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
